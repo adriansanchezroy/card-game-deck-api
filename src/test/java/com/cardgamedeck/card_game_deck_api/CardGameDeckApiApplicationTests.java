6ffa2f26c629d317b5b0ad9b4bfc669159fee170
Adrian Sanchez Roy <adriansanchezroy@gmail.com>
2025-03-01 10:58:43 -0500

    Initial Spring boot project setup

---
+++ src/test/java/com/cardgamedeck/card_game_deck_api/CardGameDeckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardgamedeck.card_game_deck_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameDeckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
